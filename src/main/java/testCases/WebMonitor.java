4851e0c3984f69a199490bc8e596eef7129eb778
jeyamurn <jeyamurn@01HW1061198.ALU.COM>
2018-03-21 13:06:25 +0530

    code included to write meminfo to a text file

--- src/main/java/testCases/WebMonitor.java
+++ src/main/java/testCases/WebMonitor.java
@@ -1,5 +1,12 @@
 package testCases;
 
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
@@ -8,38 +15,42 @@ import org.testng.annotations.Test;
 import wdMethods.ProjectMethods;
 
 public class WebMonitor extends ProjectMethods{
-	
+
 	int i = 0;
-	@Test(groups="smoke",dependsOnGroups="regression")
+	@Test(groups="smoke") //,dependsOnGroups="regression")
 	public void monitor() throws Exception {
 
 		//for (int i=0; i<=2; i++) {
-			
-			Thread.sleep(3000);
-			webappMonitor();
-			WebElement selectsys = locateElement("xpath", "(//a[@class = 'dropdown-toggle'])[3]");
-			click(selectsys);
-			Thread.sleep(3000);
-			WebElement memInfo = locateElement("link", "Memory Info");
-			click(memInfo);
-			Thread.sleep(2000);
-			takeSnap();
-			scrollDown();
-			takeSnap();
-			Thread.sleep(2000);
-			WebElement getmem = locateElement("xpath", "//html/body/div[2]/pre");
-			String meminfo = getText(getmem);
-						
-			System.out.println(meminfo);
 
-			Thread.sleep(3000);
-			closeBrowser();
-			//Thread.sleep(600000);
-			//Thread.sleep(1800000);
-				
-
-			//}
+		Thread.sleep(3000);
+		webappMonitor();
+		WebElement selectsys = locateElement("xpath", "(//a[@class = 'dropdown-toggle'])[3]");
+		click(selectsys);
+		Thread.sleep(3000);
+		WebElement memInfo = locateElement("link", "Memory Info");
+		click(memInfo);
+		Thread.sleep(2000);
+		takeSnap();
+		scrollDown();
+		takeSnap();
+		Thread.sleep(2000);
+		WebElement getmem = locateElement("xpath", "//html/body/div[2]/pre");
+		String meminfo = getText(getmem);
+		System.out.println(meminfo);
 		
-			
+		FileOutputStream out = new FileOutputStream("C://Users//jeyamurn//.jenkins//workspace//Traffic.txt");
+		out.write(meminfo.getBytes());
+		out.close();
 
-}}
+
+		Thread.sleep(3000);
+		closeBrowser();
+		//Thread.sleep(600000);
+		//Thread.sleep(1800000);
+
+
+		//}
+
+
+
+	}}
